21d079140f30734f912f8ab571c389a29535a42b
tdiccccc <the.earth3121@gmail.com>
2022-12-22 00:10:23 +0900

    [Add]ListViewに配列の値を表示させる

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -3,48 +3,61 @@ import 'package:flutter/material.dart';
 import 'package:helloworld/second_page.dart';
 
 class FirstPage extends StatelessWidget {
-  String nameText = '';
+  final List<String> entries = <String>['A', 'B', 'C', 'D'];
+  // String nameText = '';
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ファースト'),
+        title: const Text('リスト'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
-              // const Image(
-              //   image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-              // )
-              TextField(
-                onChanged: (text) {
-                  nameText = text;
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: (){
-                  // ボタン押した時に呼ばれるコードを書く
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SecondPage(nameText),
-                      fullscreenDialog: true,
-                    ),
-                  );
-                },
-                child: const Text('ボタン'),
-              ),
-            ],
-          ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 50,
+              /// 配列の値をListViewに表示する 
+              child: Center(child: Text('Entry ${entries[index]}')),
+            );
+          }
+          // separatorBuilder: (BuildContext context, int index) => const Divider(),
         ),
-      ),
+        
+      // body: Center(
+      //   child: Padding(
+      //     padding: const EdgeInsets.all(16.0),
+      //     child: Column(
+      //       children: [
+      //         Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+      //         // const Image(
+      //         //   image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+      //         // )
+      //         TextField(
+      //           onChanged: (text) {
+      //             nameText = text;
+      //           },
+      //           decoration: InputDecoration(
+      //             border: OutlineInputBorder(),
+      //           ),
+      //         ),
+      //         ElevatedButton(
+      //           onPressed: (){
+      //             // ボタン押した時に呼ばれるコードを書く
+      //             Navigator.push(
+      //               context,
+      //               MaterialPageRoute(
+      //                 builder: (context) => SecondPage(nameText),
+      //                 fullscreenDialog: true,
+      //               ),
+      //             );
+      //           },
+      //           child: const Text('ボタン'),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }

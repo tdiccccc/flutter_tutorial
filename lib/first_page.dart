48dc25c8afca4b43c693bb8d8d658dce36452b4a
tdiccccc <the.earth3121@gmail.com>
2022-12-21 23:33:07 +0900

    [Add]入力フォームの実践

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:helloworld/second_page.dart';
 
 class FirstPage extends StatelessWidget {
+  String nameText = '';
   @override
   Widget build(BuildContext context) {
     
@@ -11,18 +12,33 @@ class FirstPage extends StatelessWidget {
         title: const Text('ファースト'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            // ボタン押した時に呼ばれるコードを書く
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SecondPage(),
-                fullscreenDialog: true,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                onChanged: (text) {
+                  nameText = text;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
               ),
-            );
-          },
-          child: const Text('ボタン'),
+              ElevatedButton(
+                onPressed: (){
+                  // ボタン押した時に呼ばれるコードを書く
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SecondPage(nameText),
+                      fullscreenDialog: true,
+                    ),
+                  );
+                },
+                child: const Text('ボタン'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
+  SecondPage(this.name);
+  final String name;
+
   @override
   Widget build(BuildContext context) {
     
@@ -10,12 +13,23 @@ class SecondPage extends StatelessWidget {
         title: const Text('セカンド'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            // ボタン押した時に呼ばれるコードを書く
-            Navigator.pop(context);
-          },
-          child: const Text('前の画面へ'),
+        // mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          children: [
+            Text(
+              name,
+              style: TextStyle(
+                fontSize: 50,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: (){
+                // ボタン押した時に呼ばれるコードを書く
+                Navigator.pop(context);
+              },
+              child: const Text('前の画面へ'),
+            ),
+          ],
         ),
       ),
     );
